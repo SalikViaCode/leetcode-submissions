ca2f14d2c98a0ffab3057b4f6ae020ecc2f9e96f
Salik Sayyed <144386775+SalikViaCode@users.noreply.github.com>
2026-06-27 00:13:41 +0530

    Add: sort-colors - submission-3

---
+++ Data Structures & Algorithms/sort-colors/submission-3.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0)    count0++;
+            else if(nums[i] == 1)   count1++;
+            else count2++;
+        }
+        for(int i = 0; i < n; i++){
+            while(i < count0)   nums[i++] = 0;
+            while(i >= count0 && i < count0+count1)    nums[i++] = 1;
+            while(i >= count0+ count1 && i < n) nums[i++] = 2;
+        }
+
+    }
+}
