6c00a7bfe5bbf87d40437caf89830b8842e1f5fe
Salik Sayyed <144386775+SalikViaCode@users.noreply.github.com>
2026-07-03 00:41:45 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        int l = 0;
+        int r = n-1;
+        while(l < r){
+            while(l < r && !alphaNum(s.charAt(l))){
+                l++;
+            }
+            while(r > l && !alphaNum(s.charAt(r))){
+                r--;
+            }
+            while(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r))){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    public boolean alphaNum(char a){
+        return (a >= 'a' && a <= 'z' || a >= 'A' && a <= 'Z' || a >= '0' && a <= '9');
+    }
+}
