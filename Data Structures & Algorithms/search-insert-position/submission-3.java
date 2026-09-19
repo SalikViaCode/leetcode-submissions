9bc42d9fd517a3baf5b292956fb58e07aa0512a1
Salik Sayyed <144386775+SalikViaCode@users.noreply.github.com>
2026-09-19 23:51:39 +0530

    Add: search-insert-position - submission-3

---
+++ Data Structures & Algorithms/search-insert-position/submission-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int l = 0, r = n-1;
+        while(l <= r){
+            int m = (l+r)/2;
+            if(nums[m]==target) return m;
+            if(nums[m] > target)    r = m-1;
+            else l = m+1;
+        }
+        return l;
+    }
+}
